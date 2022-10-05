ff8ee6db57567c95cc496a79b3dc45ce1554a999
Dilshan Fernando (BB) <dilshan@backbase.com>
2022-10-05 13:47:17 +0200

    rename the paths for screenshots

--- app/src/androidTest/java/com/qa/android/allure_android/util/ScreenRecordingRules.kt
+++ app/src/androidTest/java/com/qa/android/allure_android/util/ScreenRecordingRules.kt
@@ -6,14 +6,14 @@ import com.qa.android.allure_android.rules.ScreenRecordingRuleChain
 import com.qa.android.allure_android.rules.ScreenShotRuleChain
 import org.junit.rules.RuleChain
 
-private const val MODEL_BANK_FAILURES_PATH = "AllureAndroid/failures"
+private const val FAILURES_PATH = "AllureAndroid/failures"
 private val externalStorageDirectoryPath = Environment.getExternalStorageDirectory().path
 
 private val screenshotsRelativePath =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) Environment.DIRECTORY_SCREENSHOTS else "Screenshots"
 
 private val SCREENSHOT_FAILURE_PATH =
-    "$externalStorageDirectoryPath/${Environment.DIRECTORY_PICTURES}/$screenshotsRelativePath/$MODEL_BANK_FAILURES_PATH/"
+    "$externalStorageDirectoryPath/${Environment.DIRECTORY_PICTURES}/$screenshotsRelativePath/$FAILURES_PATH/"
 
 /**
  * Factory method that returns a [RuleChain] using [ScreenRecordingRuleChain] method from `journey-test` library.
@@ -25,4 +25,4 @@ fun ScreenRecordingRuleChain(): RuleChain = ScreenRecordingRuleChain(SCREENSHOT_
  * Factory method that returns a [RuleChain] using [ScreenShotRuleChain] method from `journey-test` library.
  */
 @Suppress("FunctionName") // factory method
-fun ScreenShotRuleChain(): RuleChain = ScreenShotRuleChain(MODEL_BANK_FAILURES_PATH)
+fun ScreenShotRuleChain(): RuleChain = ScreenShotRuleChain(FAILURES_PATH)
